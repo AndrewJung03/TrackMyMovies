5e4f7a443c2183c1b903904b8dbe1a0b379fa4ca
Andrew Jung <andrew@10-17-15-7.dynapool.wireless.nyu.edu>
2024-10-31 16:45:06 -0400

    milestone 1 done

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const user = new mongoose.Schema({
   username: { type: String, required: true },
   hash: { type: String, required: true },
+  moviesWatched: {type: Array}
 });
 
 export const User = mongoose.model('User', user);
